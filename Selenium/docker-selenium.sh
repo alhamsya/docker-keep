e8c838cf768ad3a6a707d385031b71d4c6510c8c
alhamsya <alhamsya.dyasta@tokopedia.com>
2020-06-21 22:35:17 +0700

    update selenium

---
+++ Selenium/docker-selenium.sh
@@ -0,0 +1,3 @@
+docker network connect --ip 182.28.0.7 docker-network selenium-hub-test
+docker network connect --ip 182.28.0.8 docker-network selenium-chrome-test
+docker network connect --ip 182.28.0.9 docker-network selenium-firefox-test
--- docker-run.sh
+++ docker-run.sh
@@ -3,3 +3,16 @@ docker run --detach --name=mysql-test --net docker-network --ip 182.28.0.2 --env
 docker run --detach --name=redis-test --net docker-network --ip 182.28.0.3 redis
 
 docker run --detach --name=postgres-test --net docker-network --ip 182.28.0.4 --env="POSTGRES_PASSWORD=alhamsya" postgres
+
+docker run -d \
+    --name sonarqube-test \
+    --net docker-network \
+    --ip 182.28.0.5 \
+    -p 9000:9000 \
+    -v sonarqube_conf:/opt/sonarqube/conf \
+    -v sonarqube_extensions:/opt/sonarqube/extensions \
+    -v sonarqube_logs:/opt/sonarqube/logs \
+    -v sonarqube_data:/opt/sonarqube/data \
+    sonarqube:7.9-community
+
+docker run --detach --name=metabase-test --net docker-network --ip 182.28.0.6 -p 3000:3000 metabase/metabase    
