1358e7604c38d20cdaaae24fe02b330b37724941
alhamsya.dyasta <alhamsya.dyasta@stockbit.com>
2022-03-02 23:13:47 +0700

    chore(add): add redis and vault

--- docker-run-mac.sh
+++ docker-run-mac.sh
@@ -1,27 +1,44 @@
 
 docker network create --driver bridge --subnet 182.28.0.0/16 --gateway 182.28.0.1 docker-network
 
-docker run --detach --name=mysql-test --net docker-network --ip 182.28.0.2 --publish 3306:3306 --volume mysql-volume:/var/lib/mysql --env="MYSQL_ROOT_PASSWORD=alhamsya" mysql:5.7.25
+docker run --detach --name=mysql-test --net docker-network --ip 182.28.0.2 --publish 3306:3306 --volume mysql-volume:/var/lib/mysql --env="MYSQL_ROOT_PASSWORD=alhamsya" --platform linux/amd64 mysql:5.7
 
 docker run --detach --name=redis-test --net docker-network --ip 182.28.0.3 --publish 6379:6379 --privileged=true --volume redis-volume:/data redis --requirepass  "alhamsya"
 
 docker run --detach --name=postgres-test --net docker-network --ip 182.28.0.4 --publish 5432:5432 --volume postgres-volume:/var/lib/postgresql/data --env="POSTGRES_PASSWORD=alhamsya" postgres
 
 docker run --detach \
-    --name sonarqube-test \
-    --net docker-network \
-    --ip 182.28.0.5 \
-    --publish 9000:9000 \
-    --volume sonarqube-volume_conf:/opt/sonarqube/conf \
-    --volume sonarqube-volume_extensions:/opt/sonarqube/extensions \
-    --volume sonarqube-volume_logs:/opt/sonarqube/logs \
-    --volume sonarqube-volume_data:/opt/sonarqube/data \
-    sonarqube:7.9-community
+--name sonarqube-test \
+--net docker-network \
+--ip 182.28.0.5 \
+--publish 9000:9000 \
+--volume sonarqube-volume_conf:/opt/sonarqube/conf \
+--volume sonarqube-volume_extensions:/opt/sonarqube/extensions \
+--volume sonarqube-volume_logs:/opt/sonarqube/logs \
+--volume sonarqube-volume_data:/opt/sonarqube/data \
+sonarqube:7.9-community
 
-docker run --detach --name=metabase-test --net docker-network --ip 182.28.0.6 --publish 3000:3000 --volume metabase-volume:/metabase-data metabase/metabase  
+docker run --detach --name=metabase-test --net docker-network --ip 182.28.0.6 --publish 3000:3000 --volume metabase-volume:/metabase-data --platform linux/amd64 metabase/metabase
 
 docker run --detach --name=rabbitmq-test --net docker-network --ip 182.28.0.7 --publish 15672:15672 --publish 5672:5672 --volume rabbitmq-volume:/var/lib/rabbitmq rabbitmq:3-management
 
 docker run --detach --name=toxiproxy-test --net docker-network --ip 182.28.0.8 --publish 8474:8474 shopify/toxiproxy
 
 docker run --detach --name=jaeger-test --net docker-network --ip 182.28.0.9 --publish 6831:6831/udp --publish 16686:16686 --volume jaeger-volume:/tmp --env="JAEGER_AGENT_HOST=jaeger-test" jaegertracing/all-in-one:latest
+
+docker run --detach \
+--name=vault-test \
+--net docker-network \
+--ip 182.28.0.10 \
+--publish 8200:8200 \
+--volume vault-volume:/vault/logs \
+--volume vault-volume:/vault/file \
+--cap-add=IPC_LOCK -e 'VAULT_DEV_ROOT_TOKEN_ID=alhamsya' -e 'VAULT_DEV_LISTEN_ADDRESS=0.0.0.0:8200' vault
+
+docker run --detach \
+--name=redis-old-test \
+--net docker-network \
+--ip 182.28.0.11 \
+--publish 6379:6379 \
+--volume redis-volume-old:/data \
+redis:5.0
